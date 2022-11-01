81673187ef0c98c2380aad4a0fc218f944d34046
Beatriz Fernandes <93793289+Beatrizfernan@users.noreply.github.com>
2022-10-31 22:04:00 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace exercicio3_csharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor inteiro");
+            int A = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor inteiro");
+            int B = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor inteiro");
+            int C = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite um valor inteiro");
+            int D = int.Parse(Console.ReadLine());
+
+
+            int DIFERENCA = (A * B - C * D);
+
+            Console.WriteLine("DIFERENCA  = " + DIFERENCA);
+
+        }
+    }
+}
